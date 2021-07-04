05f42922307c6284d1982486e54898839079680d
cosmicbread <prayujtuli@hotmail.com>
2021-07-03 21:09:38 -0400

    Removed socket.io

--- src/server.js
+++ src/server.js
@@ -1,23 +1,10 @@
 const path = require('path')
 const app = require('express')()
 const http = require('http').Server(app)
-const io = require('socket.io')(http, {
-    cors: {
-        origin: 'http://47.204.224.76:2000',
-        //origin: 'http://192.168.86.29:2000',
-        methods: ['GET', 'POST']
-    }    
-})
 const port = 8000
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
-})
-
-io.on('connection', socket => {
-    console.error('socket.io connection')
-    for (var t = 0; t < 3; t++)
-        setTimeout(() => socket.emit('message', 'message from server'), 1000 * t)
 })
 
 http.listen(port, () => {
