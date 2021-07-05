067d3857980fed349a3f4d3b1d2b9c0be89ab5d6
cosmicbread <prayujtuli@hotmail.com>
2021-07-05 15:25:50 -0400

    Added firebase and created test game

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,11 +1,83 @@
 <script>
-    export let name;
+    export let database;
+
+    var gameStarted = false;
+    let username;
+    var color;
+    let users = {};
+
+    const initialize = () => {
+        color = '#'+(Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0');
+        database.ref('users/' + username).set({
+            x: Math.floor(Math.random() * 1000),
+            y: Math.floor(Math.random() * 1000),
+            color: color
+        });
+        gameStarted = true;
+    }
+
+    const close = (event) => {
+        database.ref('users/' + username).remove();
+    }
+
+    const handleKeydown = (event) => {
+        let key = event.key;
+        let pixels = 10;
+        if (key == 's' && gameStarted) {
+            database.ref('users/' + username).transaction(user => ({
+                ...user,
+                y: user.y + pixels
+            })
+            );
+        }
+        else if (key == 'w' && gameStarted) {
+            database.ref('users/' + username).transaction(user => ({
+                ...user,
+                y: user.y - pixels
+            })
+            );
+        }
+        else if (key == 'd' && gameStarted) {
+            database.ref('users/' + username).transaction(user => ({
+                ...user,
+                x: user.x + pixels
+            })
+            );
+        }
+        else if (key == 'a' && gameStarted) {
+            database.ref('users/' + username).transaction(user => ({
+                ...user,
+                x: user.x - pixels
+            })
+            );
+        }
+    }
+
+    database.ref('users').on('value', snapshot => {
+        const data = snapshot.val();
+        users = Object.keys(data ? data : []).map(k => ({
+            ...data[k],
+            name: k
+        }));
+    });
+
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+    {#if !gameStarted}
+        <h1>Hello!</h1>
+        <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+        <label for="username">Username:</label>
+        <input type="text" id="username" name="username" bind:value={username}><br><br>
+        <button on:click={initialize}>Start</button>
+    {:else}
+        {#each users as player}
+            <span class="dot" style="background-color:{player.color}; position: absolute; left: {player.x}px; top: {player.y}px"></span>
+        {/each}
+    {/if}
 </main>
+
+<svelte:window on:keydown={handleKeydown} on:beforeunload={close}/>
 
 <style>
 	main {
@@ -21,6 +93,13 @@
 		font-size: 4em;
 		font-weight: 100;
 	}
+
+    .dot {
+        height: 25px;
+        width: 25px;
+        border-radius: 50%;
+        display: inline-block;
+    }
 
 	@media (min-width: 640px) {
 		main {
--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,4 @@
-import App from './App.svelte';
-
-import firebase from 'firebase/app';
-import 'firebase/database';
+import firebase from "firebase/app"
 
 var firebaseConfig = {
     apiKey: "AIzaSyAV_lm-m49nT1uaXBzwBwZsXzsV16ZmdiI",
@@ -12,16 +9,9 @@ var firebaseConfig = {
     messagingSenderId: "585602058367",
     appId: "1:585602058367:web:2d7e2915b43f466e57d74a",
     measurementId: "G-LWK0DJPE26"
-};
+}
 
-firebase.initializeApp(firebaseConfig);
-const database = firebase.database();
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
 
-const app = new App({
-	target: document.body,
-	props: {
-        database: database
-	}
-});
-
-export default app;
+export const database = firebase.database()
