b1e2e9f6f9134216ad62da31bcbdacf4ae22c764
cosmicbread <prayujtuli@hotmail.com>
2021-07-19 13:49:07 -0400

    Cleared cache, removed uneccessary files

--- client/src/components/Game.svelte
+++ client/src/components/Game.svelte
@@ -1,29 +1,20 @@
 <script>
 	import Phaser from "phaser";
 
-	// const preload = () => {
-	// 	game.load.tilemapTiledJSON('map', 'assets/example_map.json')
-	// };
-
-	// const create = () => {
-	// 	const map = game.make.tilemap({ key: 'map' })
-	// };
-
-	// const update = () => {};
 	class Map extends Phaser.Scene {
 		constructor() {
 			super("Map");
 		}
 
 		preload() {
-			this.load.image("base_tiles", "assetsRPG_Nature_Tileset.png");
-			this.load.tilemapTiledJSON("tileset_nature", "assets/example_map.json");
+			this.load.image("base_tiles", "assets/RPG_Nature_Tileset.png");
+			this.load.tilemapTiledJSON("tileset_nature", "assets/embedded_map.json");
 		}
 
 		create() {
 			var map = this.make.tilemap({ key: "tileset_nature" });
 
-			const tileset = map.addTilesetImage;
+			//const tileset = map.addTilesetImage;
 		}
 
 		update() {}
@@ -31,15 +22,10 @@
 
 	const config = {
 		type: Phaser.AUTO, // Which renderer to use
-		width: 1200, // Canvas width in pixels
-		height: 900, // Canvas height in pixels
+		width: 1800, // Canvas width in pixels
+		height: 800, // Canvas height in pixels
 		parent: "game-container", // ID of the DOM element to add the canvas to
 		scene: [Map],
-		// scene: {
-		// 	preload: preload,
-		// 	create: create,
-		// 	update: update,
-		// },
 	};
 
 	const game = new Phaser.Game(config);
