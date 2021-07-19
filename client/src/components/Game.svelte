8e8cad6ad061fdc52c57789125bba2d190e8d96b
cosmicbread <prayujtuli@hotmail.com>
2021-07-19 16:57:55 -0400

    Changed PhaserJS and added grid layouts

--- client/src/components/Game.svelte
+++ client/src/components/Game.svelte
@@ -1,51 +1,28 @@
 <script>
-	import Phaser from "phaser";
+	import Map from "./Map.svelte";
+	import { Grid, Row, Column } from "carbon-components-svelte";
+	import { onMount, onDestroy, beforeUpdate, afterUpdate } from "svelte";
 
-	class Map extends Phaser.Scene {
-		constructor() {
-			super("Map");
-		}
-
-		preload() {
-			this.load.image("base_tiles", "assets/RPG_Nature_Tileset.png");
-			this.load.tilemapTiledJSON("tileset_nature", "assets/embedded_map.json");
-		}
-
-		create() {
-			// this.add.image(0, 0, 'base_tiles')
-			const map = this.make.tilemap({ key: "tileset_nature" });
-
-			const tileset = map.addTilesetImage("RPG Nature Tileset", "base_tiles");
-
-			map.createLayer("Tile Layer 1", tileset);
-
-			map.createLayer("Tile Layer 2", tileset);
-		}
-
-		update() {}
-	}
-
-	const config = {
-		type: Phaser.AUTO, // Which renderer to use
-		width: window.innerWidth, // Canvas width in pixels
-		height: window.innerHeight, // Canvas height in pixels
-		scale: {
-			mode: Phaser.Scale.FIT,
-			autoCenter: Phaser.Scale.CENTER_BOTH,
-		},
-		autoRound: false,
-		parent: "game-container", // ID of the DOM element to add the canvas to
-		scene: [Map],
-	};
-
-	const game = new Phaser.Game(config);
+	onMount(() => {
+		document
+			.getElementsByClassName("game")[0]
+			.appendChild(document.getElementsByTagName("canvas")[0]);
+	});
 </script>
 
-<div id="game-container" />
+<Grid>
+	<Row>
+		<p>Top part</p>
+	</Row>
+	<Row>
+		<Column class="game" sm={1} md={4} lg={8}>
+			<Map />
+		</Column>
+		<Column sm={1} md={1} lg={1}>
+			<p>Sidebar</p>
+		</Column>
+	</Row>
+</Grid>
 
 <style>
-	#game-container {
-		margin: 0;
-		padding: 0;
-	}
 </style>
