44e3a6cd5fb123a2d4db3d14334fb103e4864736
Prayuj Tuli <prayujtuli@hotmail.com>
2021-08-04 08:35:36 -0400

    Reverted server code to base model

--- src/connections.js
+++ src/connections.js
@@ -1,16 +1,5 @@
 let database = require('./firebase').database;
 
-database.ref('temp').on('child_added', async (snapshot) => {
-	const id = snapshot.key;
-	await sleep(2000);
-	database.ref('temp/' + id).once('value', (snapshot) => {
-		if (snapshot.exists()) {
-			database.ref('users/' + id).remove();
-			database.ref('temp/' + id).remove();
-		}
-	});
-});
-
 function sleep(ms) {
 	return new Promise((resolve) => {
 		setTimeout(resolve, ms);
